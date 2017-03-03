a0a4d12923119e5e1786adcc607c389a73344d22
Gogs <kyle@kylefrost.me>
2017-03-03 09:21:24 -0500

    fix some things here, fix some things there

--- templates/head.php
+++ templates/head.php
@@ -1,4 +1,3 @@
-<link href="/css/style.css" rel="stylesheet">
 <link href="/vendor/fa/css/font-awesome.min.css" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta name="viewport" content="width=device-width, user-scalable=no" />
@@ -8,3 +7,4 @@
 <script src="/js/moment-countdown.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script src="//unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+<link href="/css/style.css" rel="stylesheet">
