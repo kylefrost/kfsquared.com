d45ef4078fd3902d3026ae5e49f02c89fe3a6d12
Gogs <kyle@kylefrost.me>
2017-03-06 13:01:02 -0500

    accommodations page coming up last

--- accommodations/index.php
+++ accommodations/index.php
@@ -10,7 +10,9 @@
     <body>
         <?php include('../templates/google.php'); ?>
         <?php include('../templates/navbar.php'); ?>
-
+        <div class="section-ring min-full-height">
+            <div id="registry-top-text">Accommodations<div id="registry-divider"></div>
+        </div>
         <script src="/js/main.js"></script>
     </body>
 </html>
--- wedding/index.php
+++ wedding/index.php
@@ -10,6 +10,63 @@
     <body>
         <?php include('../templates/google.php'); ?>
         <?php include('../templates/navbar.php'); ?>
+        <div class="section-hero min-full-height">
+            <div id="wedding-top-text" style="overflow: visible;">10.14.17</div>
+            <div id="down-arrow" style="overflow: visible;">
+                <i class="fa fa-arrow-circle-down" style="overflow: visible;" aria-hidden="true"></i>
+            </div>
+        </div>
+        <div class="section-ring location-main">
+            <div id="location-left"></div>
+            <div id="location-right">
+                <div id="location-title">Mission San Luis</div>
+                <div id="location-text">From 1656 to 1704, San Luis served as the principal village of the Apalachees and was the Spaniards' westernmost military, religious, and administrative capital. Mission San Luis was one of over 100 mission settlements established in Spanish Florida between the 1560s and 1690s. It was home to more than 1,400 residents, including a powerful Apalachee chief and the Spanish deputy governor.<br><br>In recognition of its historical significance, San Luis received designation as a National Historic Landmark in 1960. Today Mission San Luis is the only reconstructed Spanish mission in Florida. As a living history museum it is devoted to sharing the stories of its former Apalachee and Spanish residents. Knowledge of life at San Luis over three centuries ago comes from intensive archaeological and historical research—the site is the most thoroughly investigated mission in the southeastern United States.<br><br><div id="mission-link"><a href="https://www.google.com/maps/place/Mission+San+Luis/@30.4540859,-84.32281,15.17z/data=!4m5!3m4!1s0x88ecf4f9044fa5af:0xe4e8a8bd4f2fbb84!8m2!3d30.4486819!4d-84.3200388" target="_blank"><i>2100 W Tennessee St<br>Tallahassee, FL 32304</i></a></div></div>
+            </div>
+        </div>
+        <div class="section-dark min-full-height">
+            <div id="schedule-top-text">Schedule of Events<div id="schedule-divider"></div></div>
+            <div id="schedule-lower-top-text" style="overflow: visible;">Times still subject to change.</div>
+            <section class="wedding-timeline" style="overflow: visible;">
+                <ul style="overflow: visible;">
+                    <li style="overflow: visible;">
+                        <div style="overflow: visible;">
+                            <time>4:30 PM</time><br>
+                            Begin to arrive at Mission San Luis for ceremony.
+                        </div>
+                    </li>
+                    <li style="overflow: visible;">
+                        <div style="overflow: visible;">
+                            <time>5:00 PM</time><br>
+                            Ceremony begins under <i>the Portal</i>. Attire is semi-formal.
+                        </div>
+                    </li>
+                    <li style="overflow: visible;">
+                        <div style="overflow: visible;">
+                            <time>6:00 PM</time><br>
+                            Cocktail hour in courtyard.
+                        </div>
+                    </li>
+                    <li style="overflow: visible;">
+                        <div style="overflow: visible;">
+                            <time>6:30 PM</time><br>
+                            Reception begins with introduction of wedding party in Mission Room banquet hall.
+                        </div>
+                    </li>
+                    <li style="overflow: visible;">
+                        <div style="overflow: visible;">
+                            <time>10:00 PM</time><br>
+                            Last call at bar.
+                        </div>
+                    </li>
+                    <li style="overflow: visible;">
+                        <div style="overflow: visible;">
+                            <time>10:30 PM</time><br>
+                            Mr. &amp; Mrs. send off.
+                        </div>
+                    </li>
+                </ul>
+            </section>
+        </div>
         <script src="/js/main.js"></script>
     </body>
 </html>
