d748a36b434e07743b83505587ddab6ccbac64f5
Gogs <kyle@kylefrost.me>
2017-03-06 14:58:23 -0500

    accommodations page in progress

--- accommodations/index.php
+++ accommodations/index.php
@@ -11,7 +11,90 @@
         <?php include('../templates/google.php'); ?>
         <?php include('../templates/navbar.php'); ?>
         <div class="section-ring min-full-height">
-            <div id="registry-top-text">Accommodations<div id="registry-divider"></div>
+            <div id="acc-top-text">Accommodations<div id="registry-divider"></div></div>
+            <div id="contact-lower-top-text" style="overflow: visible;">Find more information about travel and visiting.</div>
+            <div class="acc-lodging">
+                <div class="lodging-left">
+                    <div class="lodging-title">Hotels<div id="registry-divider"></div></div>
+                    <div class="lodging-text">
+                        <a href="https://www.wyndhamhotels.com/days-inn/tallahassee-florida/days-inn-tallahassee-university-center/overview?CID=LC:DI:20160927:RIO:Local:SM-disatl" target="_blank">Days Inn Tallahassee</a>
+                        <br>
+                        1350 West Tennessee St
+                        <br>
+                        Tallahassee, FL 32304
+                        <br>
+                        (850) 222-3219
+                        <br><br> 
+                        <a href="http://www.fourpointstallahasseedowntown.com/" target="_blank">Four Points by Sheraton</a>
+                        <br>
+                        316 West Tennessee St
+                        <br>
+                        Tallahassee, FL 32301
+                        <br>
+                        (850) 422-0071
+                        <br><br>
+                        <a href="http://www.marriott.com/hotels/travel/tlhak-hotel-duval-autograph-collection/?scid=bb1a189a-fec3-4d19-a255-54ba596febe2" target="_blank">Hotel Duval</a>
+                        <br>
+                        415 North Monroe St
+                        <br>
+                        Tallahassee, FL 32301
+                        <br>
+                        (850) 224-6000
+                        <br><br>
+                        <a href="http://www.alofttallahassee.com/" target="_blank">Aloft Tallahassee</a>
+                        <br>
+                        200 North Monroe St
+                        <br>
+                        Tallahassee, FL 32301
+                        <br>
+                        (850) 513-0313
+                        <br><br>
+                        <a href="http://doubletree3.hilton.com/en/hotels/florida/doubletree-by-hilton-hotel-tallahassee-THLAPDT/index.html" target="_blank">DoubleTree by Hilton</a>
+                        <br>
+                        101 South Adams St
+                        <br>
+                        Tallahassee, FL 32301
+                        <br>
+                        (850) 224-5000
+                        <br><br>
+                        <div class="extra-options">
+                            <i>Due to Florida A&amp;M University's Homecoming weekend coinciding with the wedding, affordable hotel rooms may be difficult to find. We recommend searching <a href="https://airbnb.com/" target="_blank">Airbnb.com</a> or <a href="https://vrbo.com/" target="_blank">VRBO.com</a> in the event you can't find an affordable hotel room to suit your needs.</i>
+                        </div>
+                    </div>
+                </div>
+                <div class="lodging-right">
+                    <div class="lodging-title">Airlines<div id="registry-divider"></div></div>
+                    <div class="lodging-text">
+                        <div class="extra-options"><i>These airlines fly into Tallahassee International Airport (TLH).</i></div>
+                        <br><br>
+                        <a href="https://www.aa.com/homePage.do" target="_blank">American Airlines</a>
+                        <br>
+                        (800) 433-7300
+                        <br><br>
+                        <a href="http://www.delta.com/" target="_blank">Delta Airlines</a>
+                        <br>
+                        (800) 221-1212
+                        <br><br>
+                        <a href="http://www.silverairways.com/" target="_blank">Silver Airways</a>
+                        <br>
+                        (800) 241-6522
+                        <br><br>
+                        <div class="extra-options">
+                            <i>Some may find it most cost efficient to fly into <a href="https://www.iflybeaches.com/" target="_blank">Northwest Florida Beaches International Airport (ECP)</a> in Panama City (107mi), or <a href="www.flyjacksonville.com/" target="_blank">Jacsonville International Airport (JAX)</a> (172mi). These would require a rental car for transportation to Tallahassee, and are therefore not ideal for everyone.</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="acc-lodging">
+                <div class="lodging-left">
+                    <div class="lodging-title">Transportation<div id="registry-divider"></div></div>
+                    <div class="lodging-text"></div>
+                </div>
+                <div class="lodging-right">
+                    <div class="lodging-title">Explore Tallahassee<div id="registry-divider"></div></div>
+                    <div class="lodging-text"></div>
+                </div>
+            </div>
         </div>
         <script src="/js/main.js"></script>
     </body>
